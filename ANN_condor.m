400728a805226272242962f10d999fc9cd84c942
Thomas Wood <grand.edgemaster@gmail.com>
2012-02-22 21:17:16 +0000

    Condor change

--- ANN_condor.m
+++ ANN_condor.m
@@ -1,8 +1,9 @@
-function ANN_condor( topology, learning_rate, trans_func, train_func)
+function ANN_condor( topology, learning_rate, trans_func, train_func, process)
 %ANN_CONDOR Summary of this function goes here
 
     filename = ['condor/result-', '[', num2str(topology, '%d,'), ']-', ...
-        num2str(learning_rate), '-', trans_func, '-', train_func, '.mat'];
+        num2str(learning_rate), '-', trans_func, '-', train_func, ,'-', ...
+        num2str(process), '.mat'];
     avgf1 = ANN_main(topology, learning_rate, trans_func, train_func);
     save(filename, 'avgf1');
     
