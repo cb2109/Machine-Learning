fe74325967873d8a6d05909cc15e6e38a8d83e07
Thomas Wood <tw1509@doc.ic.ac.uk>
2012-02-23 11:58:05 +0000

    CHanges to condor nicer

--- ANN_condor.m
+++ ANN_condor.m
@@ -4,6 +4,5 @@ function ANN_condor( topology, learning_rate, trans_func, train_func, process)
     avgf1 = ANN_main(topology, learning_rate, trans_func, train_func);
     
     save(['condor/result-', num2str(process)]);
-    %exit;
 end
 
