d682ee361db4d3b50513ed694094e470ce78430e
Thomas Wood <grand.edgemaster@gmail.com>
2012-02-23 03:15:46 +0000

    Tweaks to aggregator

--- ANN_condor_aggregate.m
+++ ANN_condor_aggregate.m
@@ -1,12 +1,18 @@
 function ANN_condor_aggregate
-    files = strtrim(regexp(ls('condor/*.mat'), ' +', 'split'));
+    files = strtrim(regexp(ls('condor/*.mat'), '\s+', 'split'));
     num = size(files, 2);
     
     for i = 1:num
-        rows(i,:) = load(char(files(:,i)));
+        c = char(files(:,i));
+        if c
+            rows(i,:) = load(c);
+            if length(rows(i).topology) < 2
+                rows(i).topology(1, 2) = 0;
+            end
+        end
     end
     
-    topology = reshape([rows.topology], num, 2);
+    topology = reshape([rows.topology], [], 2);
     x=topology(:,1);
     y=topology(:,2);
     z=[rows.avgf1]';
@@ -20,6 +26,9 @@ function ANN_condor_aggregate
     [X,Y]=meshgrid(x_edge,y_edge);
     
     Z = F(X,Y);
+    
     mesh(X,Y,Z);
+    hold on;
     plot3(x,y,z,'o');
+    grid on;
 end
